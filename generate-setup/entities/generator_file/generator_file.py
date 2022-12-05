13988c8821f2d995c5ba3bf92aae1c88010dc8d7
leandl <leandrocrispim891@gmail.com>
2022-12-05 20:09:20 -0300

    * formatada as descrições das questões.

--- generate-setup/entities/generator_file/generator_file.py
+++ generate-setup/entities/generator_file/generator_file.py
@@ -5,6 +5,6 @@ class GeneratorFile:
   def __init__(self, path_template: str) -> None:
     with open(path_template, "r") as file_templete:
       self._template = file_templete.read()
-
+      
   def generate(self, number_question: int, question) -> Tuple[str, str]:
     pass
