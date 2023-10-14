560e24b7a9f37751e7e8bdbd224dfeb7d8538b46
leandl <leandrocrispim891@gmail.com>
2023-10-14 20:59:56 -0300

    * Validação com dados do tipo array para .

--- validator/javascript/src/validator.js
+++ validator/javascript/src/validator.js
@@ -1,59 +1,57 @@
 const { Printer } = require("./printer");
 
 class Validator {
-    constructor(questions, questionsFunction) {
-        this.questions = questions;
-        this.questionsFunction = questionsFunction;
+  constructor(questions, questionsFunction) {
+    this.questions = questions;
+    this.questionsFunction = questionsFunction;
+  }
+
+  testAll() {
+    return Object.entries(this.questionsFunction).map(
+      ([nameQuestion, _questionFunction]) => ({
+        number: this.#getNumberQuestion(nameQuestion),
+        data: this.test(nameQuestion),
+      })
+    );
+  }
+
+  #getNumberQuestion(nameQuestion) {
+    return Number(nameQuestion.replace(/[^0-9]/g, ""));
+  }
+
+  #getIndexQuestion(nameQuestion) {
+    const numberQuestion = this.#getNumberQuestion(nameQuestion);
+    return numberQuestion - 1;
+  }
+
+  test(nameQuestion) {
+    const indexQuestion = this.#getIndexQuestion(nameQuestion);
+
+    const question = this.questions[indexQuestion];
+    const questionFunction = this.questionsFunction[nameQuestion];
+
+    if (!question) {
+      Printer.error("not existis question");
     }
 
-    testAll() {
-        return Object
-            .entries(this.questionsFunction)
-            .map(
-                ([nameQuestion, _questionFunction]) => ({
-                    number: this.#getNumberQuestion(nameQuestion),
-                    data: this.test(nameQuestion)
-                })
-            );
+    if (!questionFunction) {
+      Printer.error("not existis question function");
     }
 
-    #getNumberQuestion(nameQuestion) {
-        return Number(nameQuestion.replace(/[^0-9]/g,''));
-    }
-    
-    #getIndexQuestion(nameQuestion) {
-        const numberQuestion = this.#getNumberQuestion(nameQuestion);
-        return numberQuestion - 1;
-    }
-        
-    test(nameQuestion) {
-        const indexQuestion = this.#getIndexQuestion(nameQuestion);
+    return question.getTests().map((test) => {
+      const result = questionFunction(...test.getArgs());
+      const expectedResult = test.getResult();
 
-        const question = this.questions[indexQuestion];
-        const questionFunction = this.questionsFunction[nameQuestion];
-        
-        if (!question) {
-            Printer.error("not existis question");
-        }
-            
-        if (!questionFunction) {
-            Printer.error("not existis question function");
-        }
-
-        return question.getTests().map((test) => {
-            const result = questionFunction(...test.getArgs());
-            const expectedResult =test.getResult();
-
-            return {
-                'args': test.getArgs(),
-                'expected_result': expectedResult,
-                'result': result,
-                'passed': result === expectedResult
-            }
-        });
-    } 
+      return {
+        args: test.getArgs(),
+        expected_result: expectedResult,
+        result: result,
+        passed: JSON.stringify(result) === JSON.stringify(expectedResult),
+      };
+    });
+  }
 }
 
 module.exports = {
-    Validator
-}
+  Validator,
+};
